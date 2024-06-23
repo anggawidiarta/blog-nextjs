e7dec71d16c51bf57852500f636bc85b33ddf7f7
Angga Widiarta <anggawidiarta55@gmail.com>
2024-06-23 23:52:33 +0800

    init project on dev

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,18 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import "@/app/styles/globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: "Blog App",
+  description: "The best blog app!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="flex justify-center items-center w-full">{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div className="text-center text-red-700 text-4xl">Hello</div>;
 }
